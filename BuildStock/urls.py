0207663ba5e58cc3f5b35e02ce4b7d2fea710cc3
maimounayebhe2023 <tmaimounabarry002@gmail.com>
2026-01-14 16:01:06 +0000

    update the path swagger's path

--- BuildStock/urls.py
+++ BuildStock/urls.py
@@ -24,8 +24,8 @@ urlpatterns = [
     # JWT endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+     # Swagger UI
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 
- # Swagger UI
-path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
+]
 
