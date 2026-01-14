76f914aca510287336777d85856bc81c1367039c
maimounayebhe2023 <tmaimounabarry002@gmail.com>
2026-01-14 12:10:51 +0000

    Add user list and create API view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .userSerializers import UserSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from .models import User
+
 
 # Create your views here.
+
+class ListCreateUserApiView(APIView) :
+
+    def get(self, *args, **kwargs):
+        users=User.objects.all()
+        serializer=UserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self,request, *args, **kwargs):
+        serializer=UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user=serializer.save()
+            return Response({
+                "id": user.id,
+                "username": user.username,
+                "role": user.role
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
