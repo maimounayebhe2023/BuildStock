314db39f649f47ceb53f18aef5066a48d1883bd4
maimounayebhe2023 <tmaimounabarry002@gmail.com>
2026-01-15 11:28:50 +0000

    Fix Swagger on Render: use OpenAPISchemaGenerator to avoid coreapi errors

--- BuildStock/urls.py
+++ BuildStock/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.generators import OpenAPISchemaGenerator
 from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -14,6 +15,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
+     generator_class=OpenAPISchemaGenerator
 )
 
 urlpatterns = [
