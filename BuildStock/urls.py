4ce0eea587c4396015259566f5420d4c6def03e6
maimounayebhe2023 <tmaimounabarry002@gmail.com>
2026-01-15 12:11:47 +0000

    Migrate to OpenAPI: remove coreapi, add AutoSchema, Swagger/Redoc ready for Render

--- BuildStock/urls.py
+++ BuildStock/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from drf_yasg.generators import OpenAPISchemaGenerator
-from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-# --- Configuration Swagger / OpenAPI ---
 schema_view = get_schema_view(
     openapi.Info(
         title="BuildStock API",
@@ -15,23 +13,21 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
-     generator_class=OpenAPISchemaGenerator
 )
 
 urlpatterns = [
-    #  Admin 
+    # Admin
     path('admin/', admin.site.urls),
 
-    #  API de l'application 
+    # API utilisateurs
     path('api/', include('users.urls')),
 
-    #  JWT  
+    # JWT
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    #  Documentation Swagger 
+    # Documentation OpenAPI
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger.yaml', schema_view.without_ui(cache_timeout=0), name='schema-yaml'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
