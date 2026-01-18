16aa349a50c659ae84e797b25de517d7e644fda3
maimounayebhe2023 <tmaimounabarry002@gmail.com>
2026-01-18 17:29:26 +0000

    Handle category creation in Product POST: get or create category by name

--- products/views.py
+++ products/views.py
@@ -1,12 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status 
+from rest_framework.permissions import AllowAny
 from .models import ProductCategory, Product
 from .serializers import ProductCategorySerializer, ProductSerializer
 
 # Create your views here.
 class ProductCategoryListCreateAPIView(APIView):
-
+    permission_classes=[AllowAny]
     def get(self, request):
         categories= ProductCategory.objects.all()
         serializer=ProductCategorySerializer(categories, many=True)
@@ -20,8 +21,9 @@ class ProductCategoryListCreateAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
-    
+
 class ProductListCreateAPIView(APIView):
+    permission_classes=[AllowAny]
 
     def get(self, request):
         products = Product.objects.all()
@@ -29,7 +31,16 @@ class ProductListCreateAPIView(APIView):
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = ProductSerializer(data=request.data)
+        category_name= request.data.get('category')
+        data=request.data.copy()
+        if category_name:
+            category, created = ProductCategory.objects.get_or_create(
+                name=category_name.strip()
+            )
+            data['category']= category.id
+        else :
+            data['category'] = None
+        serializer = ProductSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
