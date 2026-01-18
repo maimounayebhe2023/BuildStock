0ac38159e5329e24e1a805a5f24d697a05035e8f
maimounayebhe2023 <tmaimounabarry002@gmail.com>
2026-01-18 17:30:32 +0000

    Add URL routes for ProductCategory and Product API endpoints

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import(ProductCategoryListCreateAPIView, ProductListCreateAPIView)
 
 urlpatterns=[
-    path('categories/', ProductCategoryListCreateAPIView.as_view),
-    path('products/', ProductListCreateAPIView.as_view )
+    path('categories/', ProductCategoryListCreateAPIView.as_view()),
+    path('products/', ProductListCreateAPIView.as_view() )
 ]
